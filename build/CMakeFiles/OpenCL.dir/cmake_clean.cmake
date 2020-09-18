0ac4975f5cc03ad23c917b35be2bd943af908465
moarseniy <moarseniy@yandex.ru>
2020-09-18 13:41:27 +0300

    Test CMake!

---
+++ build/CMakeFiles/OpenCL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCL.dir/sources/main.cpp.o"
+  "OpenCL.pdb"
+  "OpenCL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
